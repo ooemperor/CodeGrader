f861ba656a1a9da5a2a23acb45fcc0f8dd7ad5a1
Michael Kaiser <mi.kaiser@bluewin.ch>
2023-09-26 14:02:27 +0200

    adding first draft of constructor of the Base Class

--- src/backend/db/Base.py
+++ src/backend/db/Base.py
@@ -17,22 +17,42 @@ class Base(object):
         Base Constructor of an Object in the database
         @param args: multiple input arguments
         @param kwargs: multiple input keyword:arguments
+        @raise: AttributeError when too many values are provided to the object
         """
+
+
+        # check if
+        if len(kwargs.keys()) > len(self._col_props):
+            raise AttributeError("More values provided than Object supports")
+
+        # might wanna catch the error here
+        self.set_attrs(kwargs)
+
+
 
     def set_attrs(self, attributes, fill_with_defaults=True):
         """
-
+        Setting the attributes for a object defined by the attributes
         @param attributes: the attributes of the object that shall be set
+        @type attributes: Dictionary
         @param fill_with_defaults: If empty Columns shall be filled with the default values of the database
+        @type fill_with_defaults: Boolean
         @return: True of successful, false if error
         """
         # attributes = attributes.copy()
         # TODO: implement function
         return True
 
+
     def get_attrs(self):
         """
         Getting a dictionary of all the arguments for a given Object in the Database
-        @return: The dictionary with the attributes.
+        @return: The dictionary with the attributes and values that define the Object on which the function is called.
         """
-        # TODO: implement function
+        data = dict()
+
+        # go through all columns and add them to the data dictionary
+        for val in self._col_props:
+            data[val.key] = getattr(val)
+
+        return data
