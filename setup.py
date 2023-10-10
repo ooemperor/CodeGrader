533d080747b567f3cb3eaaf5a1f20ffe235810c6
Michael Kaiser <mi.kaiser@bluewin.ch>
2023-10-10 16:56:11 +0200

    changing setup file

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(
     name='CodeGrader',
     version='1.0',
-    packages=['src', 'src.backend', 'src.backend.db', 'src.backend.config', 'src.backend.logger',
-              'src.backend.handlers'],
-    url='',
+    packages=['src/', 'src.backend/', 'src.backend.db/', 'src.backend.config/', 'src.backend.logger/',
+              'src.backend.handlers/'],
+    url='https://github.com/ooemperor/CodeGrader',
     license='',
     author='mkaiser',
     author_email='',
