3a1bd1931d368238c96108409c366a4b585e238b
Michael Kaiser <mi.kaiser@bluewin.ch>
2023-10-19 13:43:15 +0200

    adding unit test for the Submisson

---
+++ tests/backend/unit_tests/test_Submission.py
@@ -0,0 +1,72 @@
+"""
+Class for Testcases for the codeGrader.backend.db.Submission Class
+@author: mkaiser
+"""
+import unittest
+from codeGrader.backend.db import Task, Submission, User, Session
+from sqlalchemy import select
+
+class SubmissionTest(unittest.TestCase):
+    def test_SubmissionCreation(self):
+        """
+        Test if the creation of a Submission in the Database works correctly and if the Submission can be correctly read from the database
+        @return: No return value
+        """
+
+        # creating session, user and task that we need in order to test the Submission.
+        sql = Session()
+        taskdict = {
+            "name": "task2",
+            "tag": "tasktag"
+        }
+
+        task = Task(**taskdict)
+        sql.create(task)
+
+        userdict = {
+            "username": "tuser1",
+            "first_name": "test",
+            "last_name": "user",
+            "email": "test.user@mail.com",
+            "password": "myPassword",
+            "tag": "usertag"
+        }
+
+        user = User(**userdict)
+        sql.create(user)
+
+        # getting the object out of the database
+        with sql.session.begin() as session:
+            task_id = session.scalars(select(Task.id).where(Task.name == "task2")).one()
+            task = session.get(Task, task_id)
+
+            user_id = session.scalars(select(User.id).where(User.username == "tuser1")).one()
+            user = session.get(User, user_id)
+
+            submission_dict = dict()
+            submission_dict["task_id"] = task.id
+            submission_dict["user_id"] = user.id
+            submission = Submission(**submission_dict)
+
+            sql.create(submission)
+            sub_id = session.scalars(select(Submission.id).where(Submission.task_id == task_id and Submission.user_id == user_id)).one()
+            submission = session.get(Submission, sub_id)
+
+            # basic assertions on the submission
+            self.assertEqual(user_id, submission.user_id)
+            self.assertEqual(task_id, submission.task_id)
+            self.assertEqual(user, submission.user)
+            self.assertEqual(task, submission.TaskSubmission)
+
+            # testing the correctnes of the user and task that are referenced from the submission
+            self.assertEqual("task2", submission.TaskSubmission.name)
+            self.assertEqual("tasktag", submission.TaskSubmission.tag)
+            self.assertEqual("tuser1", submission.user.username)
+            self.assertEqual("test", submission.user.first_name)
+            self.assertEqual("user", submission.user.last_name)
+            self.assertEqual("usertag", submission.user.tag)
+
+            # deleting the objects after the tests
+            session.delete(submission)
+            session.delete(task)
+            session.delete(user)
