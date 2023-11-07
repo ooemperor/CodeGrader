535f1474d7e1e3b9110da58d7f058fb722f27294
Michael Kaiser <mi.kaiser@bluewin.ch>
2023-11-07 20:33:39 +0100

    reverting requirements changes and adding new build scripts for better deployment

---
+++ setup_backend.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='codeGrader',
+    version='1.0',
+    packages=find_packages(exclude=["*frontend*"]),
+    url='https://github.com/ooemperor/CodeGrader',
+    license='',
+    author='mkaiser',
+    author_email='',
+    description='',
+    entry_points={
+        'console_scripts': [
+            'cgDeployDB = codeGrader.scripts.deployDB:main',
+            'cgEvaluationService = codeGrader.scripts.cgEvaluationService:main',
+            'cgExecutionService = codeGrader.scripts.cgExecutionService:main'
+        ]
+    }
+)
---
+++ setup_frontend.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='codeGrader',
+    version='1.0',
+    packages=find_packages(exclude=["*backend*"]),
+    url='https://github.com/ooemperor/CodeGrader',
+    license='',
+    author='mkaiser',
+    author_email='',
+    description='',
+)
