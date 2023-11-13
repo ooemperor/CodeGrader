82f1f3eb42b0e10ff0f6a319a7bdba178f1be533
Michael Kaiser <mi.kaiser@bluewin.ch>
2023-11-13 18:11:38 +0100

    fixing some variables in order to run code on server

--- setup_frontend.py
+++ setup_frontend.py
@@ -11,6 +11,7 @@ setup(
     name='codeGrader',
     version='1.0',
     packages=find_packages(exclude=["*backend*"]),
+    package_data=PACKAGE_DATA,
     url='https://github.com/ooemperor/CodeGrader',
     license='',
     author='mkaiser',
