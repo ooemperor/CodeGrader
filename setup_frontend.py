d9bfcb2d54a1b7d760c097fff6111ba7d1b40ba6
Michael Kaiser <mi.kaiser@bluewin.ch>
2023-11-13 18:13:27 +0100

    fixing some variables in order to run code on server

--- setup_frontend.py
+++ setup_frontend.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 PACKAGE_DATA = {
-    "codeGrader.frontend.admin":[
-        "/static/*.*",
-        "/templates/*.*"
+    "codeGrader.frontend": [
+        "admin/static/*.*",
+        "admin/templates/*.*"
     ]
 }
 
