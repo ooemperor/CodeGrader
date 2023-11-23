2d2a3bcba4e29a2012d6db2e79e1b2db4aa1c23d
Michael Kaiser <mi.kaiser@bluewin.ch>
2023-11-23 18:12:25 +0100

    pushing the close nav button in admin frontend into base template, adding support for flashing messaging via base template.

--- codeGrader/frontend/admin/app.py
+++ codeGrader/frontend/admin/app.py
@@ -3,7 +3,7 @@ Route definition and main File for the Admin Frontend of the CodeGrader
 @author: mkaiser
 @version: 1.0
 """
-from flask import Flask, request, render_template, url_for, redirect
+from flask import Flask, request, render_template, url_for, redirect, flash
 from flask_login import LoginManager, login_user, login_required, logout_user
 from codeGrader.frontend.config import config
 from codeGrader.frontend.admin import templates
