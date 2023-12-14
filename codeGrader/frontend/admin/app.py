5304c3e0ec92f823870de51c73e9dbaa664660ce
Michael Kaiser <mi.kaiser@bluewin.ch>
2023-12-14 16:37:32 +0100

    chaning flask application to use gevent wsgi server instead of gunicorn

--- codeGrader/frontend/admin/app.py
+++ codeGrader/frontend/admin/app.py
@@ -16,6 +16,7 @@ from codeGrader.frontend.admin.handlers import AdminUserLoginHandler, AdminSessi
     AddSubjectHandler, DeleteUserHandler, DeleteSubjectHandler, DeleteAdminHandler, DeleteTaskHandler, \
     DeleteExerciseHandler, DeleteProfileHandler, AddTaskAttachmentHandler, DeleteTaskAttachmentHandler, \
     AddTaskInstructionHandler, DeleteTaskInstructionHandler, TaskInstructionHandler, TaskAttachmentHandler
+from gevent.pywsgi import WSGIServer
 
 app = Flask(config.adminAppName, template_folder=templates.__path__[0])
 
@@ -506,4 +507,6 @@ def deleteTaskInstruction(task_id_: int, instruction_id_: int) -> Union[Response
 
 
 if __name__ == "__main__":
-    app.run(port=config.adminPort)
+    http_server = WSGIServer(("127.0.0.1", int(config.adminPort)), app)
+    http_server.serve_forever()
+    # app.run(port=config.adminPort)
