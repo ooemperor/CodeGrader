5f0730cc4609a9554ecc4ea336eec7aadd452ec5
Michael Kaiser <mi.kaiser@bluewin.ch>
2024-02-18 13:39:15 +0100

    updating the run script for the user frontend and reverting to stock deplyment

--- codeGrader/scripts/__init__.py
+++ codeGrader/scripts/__init__.py
@@ -21,6 +21,3 @@ Init File for the scripts that we will use to generate the Terminal commands.
 @author: mkaiser
 """
 
-# from . import deployDB, cgEvaluationService, cgExecutionService, cgApiBackend
-
-# __all__ = ["deployDB", "cgEvaluationService", "cgExecutionService", "cgAdminFrontend", "cgApiBackend"]
