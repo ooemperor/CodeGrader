7d5bd1f51b681802c028cc4342ac7d906734c3e8
Michael Kaiser <mi.kaiser@bluewin.ch>
2024-02-18 12:00:33 +0100

    trying to fix non working terminal script for adding admin

--- setup_backend.py
+++ setup_backend.py
@@ -33,7 +33,7 @@ setup(
             'cgEvaluationService = codeGrader.scripts.cgEvaluationService:main',
             'cgExecutionService = codeGrader.scripts.cgExecutionService:main',
             'cgAddApiToken = codeGrader.scripts.cgAddApiToken:main',
-            'cgAddAdmin = codeGrader.scripts.cgAddAdmin:main',
+            'cgAddAdmin = codeGrader.scripts.cgAddAdmin:main(*args)',
             'cgApiBackend = codeGrader.backend.api.app:api_backend'
 
         ]
